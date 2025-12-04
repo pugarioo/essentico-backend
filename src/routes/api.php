a5e4882f49e53d449226e5e8b396bf3c16084a97
Joshua Lopez <lopezjoshuapanaligan@gmail.com>
2025-12-04 22:03:30 +0800

    added middleware for auth

--- src/routes/api.php
+++ src/routes/api.php
@@ -36,7 +36,12 @@ Route::apiResource('order-items', OrderItemController::class);
 
 // Auth routes
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'user']);
+});
 
 /*
 |--------------------------------------------------------------------------
